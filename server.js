c90d24645fcb59a8ac45842519941639159ecd8d
kubilayelmas35 <100069033+kubilayelmas35@users.noreply.github.com>
2026-03-21 00:19:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors    = require('cors');
 const path    = require('path');
 const https   = require('https');
+const axios   = require('axios');
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
